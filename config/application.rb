79b0b0100a172e0d29a2f4c34e345a0a37f930f8
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-03 02:19:19 +0900

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails7Template
+module Kc3CheckinApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0

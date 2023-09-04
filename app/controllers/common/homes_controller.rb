0a090412b9334023bc99a5b897018d87e14119f8
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-04 14:25:42 +0900

    add: topのリダイレクト分岐処理

--- app/controllers/common/homes_controller.rb
+++ app/controllers/common/homes_controller.rb
@@ -1,4 +1,14 @@
 class Common::HomesController < ApplicationController
   def top
+    if Admin.logged_in(session)
+      # 管理者ログイン済ならadmin/events#indexへ
+      redirect_to admin_events_path
+    elsif User.logged_in(session)
+      # 顧客ログイン済ならマイページへ
+      redirect_to my_page_path
+    else
+      # その他はauthへ
+      redirect_to auth_path
+    end
   end
 end

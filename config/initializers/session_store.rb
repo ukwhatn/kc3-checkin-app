d740f4f6471e0fc69791cc555467390cd81cb1e6
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-05 21:54:12 +0900

    fix: セッションが12h保つように変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ domain = ENV.fetch("APP_DOMAIN", "localhost")
 
 Rails.application.config.session_store :redis_store,
                                        servers: %w(redis://redis:6379/0/session),
-                                       expire_in: 90.minutes,
+                                       expire_in: 12.hours,
                                        key:,
                                        domain:,
                                        secure:,

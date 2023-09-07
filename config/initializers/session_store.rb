0f0486bc38e70bf0a6c4e6496f93bb7922c61a39
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-07 12:34:56 +0900

    fix: koreha?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
-secure = false
+secure = Rails.env.production?
 key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
-domain = ENV.fetch("APP_DOMAIN", "localhost")
+domain = ENV.fetch("APP_HOST", "localhost")
 
 Rails.application.config.session_store :redis_store,
                                        servers: %w(redis://redis:6379/0/session),

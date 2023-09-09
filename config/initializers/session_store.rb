0fd4ffb623858f0e69df100e8128fa2c48551970
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-09 16:44:13 +0900

    fix: redis session storeの設定変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,18 @@ secure = Rails.env.production?
 key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
 domain = ENV.fetch("APP_HOST", "localhost")
 
-Rails.application.config.session_store :redis_store,
-                                       servers: %w(redis://redis:6379/0/session),
-                                       expire_in: 4.days,
-                                       key:,
-                                       domain:,
-                                       secure:,
-                                       httponly: true
+# Rails.application.config.session_store :redis_store,
+#                                        servers: %w(redis://redis:6379/0/session),
+#                                        expires_in: 4.days,
+#                                        key:,
+#                                        domain:,
+#                                        secure:,
+#                                        httponly: true
+Kc3CheckinApp::Application.config.session_store :redis_store,
+                                                servers: ["redis://redis:6379/0/session"],
+                                                expire_after: 4.days,
+                                                key:,
+                                                domain:,
+                                                threadsafe: true,
+                                                secure:,
+                                                httponly: true
